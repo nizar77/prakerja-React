6bd8dc940eb6855e52a9bf4cb685477fee6703be
smkTexmacoRpl <fifahnadia@gmail.com>
2024-07-25 18:35:05 +0700

    component ReactJs

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import CompHeader from "./component/header";
+import Hero from "./component/hero";
+import About from "./component/About";
+import Service from "./component/Service";
+import Portfolio from "./component/Portfolio";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <>
+      <CompHeader />
+      <Hero />
+      <About /> 
+     <Portfolio></Portfolio>
+     <Contact></Contact>
+     <Footer />
+    </>
+  );
+}
+
+export default App;
